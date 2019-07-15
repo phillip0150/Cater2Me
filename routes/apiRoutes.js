560e66cd084906a9358f504b203690e8d468125e
Phillip Valdez <Phillip0150@gmail.com>
2019-07-15 16:04:23 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,66 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  
+  //Create user
+  app.post("/api/createUser", function(req, res) {
+    db.User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(function(caterdb) {
+      res.json(caterdb);
+    });
+  });
+  
+  //Create vendor
+  app.post("/api/createUser", function(req, res) {
+    db.Vendor.create({
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+      password: req.body.password
+    }).then(function(caterdb) {
+      res.json(caterdb);
+    });
+  });
+  
+  //Geting all events
+  app.get("/api/events", function(req, res){
+    db.Event.findAll({}).then(function(caterdb){
+      res.json(caterdb);
+    });
+  });
+  
+  //Create Event
+  app.post("/api/createEvent/:id", function(req, res) {
+    db.Event.create({
+      userid: req.params.id,
+      phone: req.body.phone,
+      city: req.body.city,
+      state: req.body.state,
+      occasion: req.body.occasion,
+      courses: req.body.courses,
+      consideration: req.body.consideration,
+      size: req.body.size,
+      alcohol: req.body.size,
+      decor: req.body.decor,
+      comments: req.body.comments
+    }).then(function(caterdb) {
+      res.json(caterdb);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //Look at event
+  app.get("/api/event/:id", function(req, res) {
+    db.Event.findOne({
+      where: {
+        eventid: req.params.id
+      }
+    }).then(function(caterdb) {
+      res.json(caterdb);
     });
   });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  
+  
 };
