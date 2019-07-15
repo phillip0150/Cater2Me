2d73c69d61ea093671ec14c277ab38e4623ef98f
lukas simianer <lsimianer@gmail.com>
2019-07-15 15:22:16 -0500

    fixed

--- models/user.js
+++ models/user.js
@@ -1,34 +1,34 @@
 module.exports = function (sequelize, DataTypes) {
-var User = sequelize.define("User", {
-  userid: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [1]
+  var User = sequelize.define("User", {
+    userid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      }
     }
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [1],
-      isEmail: true
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [1],
-    }
-  }
-});
+  });
 
-return User;
+  return User;
 };
--- models/vendor.js
+++ models/vendor.js
@@ -1,40 +1,40 @@
 module.exports = function (sequelize, DataTypes) {
-var Vendor = sequelize.define("Vendor", {
+  var Vendor = sequelize.define("Vendor", {
     vendorid: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     phone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1],
-            isEmail: true
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        isEmail: true
+      }
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1],
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      }
     }
-});
-    return Vendor;
+  });
+  return Vendor;
 };
