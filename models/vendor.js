dacb5e94206081ddd42d9b4b112a3a2241376026
lukas simianer <lsimianer@gmail.com>
2019-07-15 15:11:23 -0500

    travis ci is dumb

--- models/vendor.js
+++ models/vendor.js
@@ -1,41 +1,40 @@
 module.exports = function (sequelize, DataTypes) {
-    var Vendor = sequelize.define("Vendor", {
-        vendorid: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        phone: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1],
-                isEmail: true
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1],
-            }
+var Vendor = sequelize.define("Vendor", {
+    vendorid: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
         }
-    });
-
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1],
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1],
+        }
+    }
+});
     return Vendor;
 };
