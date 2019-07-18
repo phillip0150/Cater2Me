1543f7bcd59dfff22d382befb159fdd385564dfb
Phillip Valdez <Phillip0150@gmail.com>
2019-07-18 10:28:28 -0500

    Update vendor-home.js

--- public/js/vendor-home.js
+++ public/js/vendor-home.js
@@ -1,36 +1,36 @@
 // $(document).ready(function () {
-    var placeArray = (window.location.pathname).split('/');
-    var vendorId = placeArray.slice(-1)[0];
-    console.log("VENDOR ID OUTSIDE CLICK FUNCTION: " + vendorId);
+var placeArray = (window.location.pathname).split("/");
+var vendorId = placeArray.slice(-1)[0];
+console.log("VENDOR ID OUTSIDE CLICK FUNCTION: " + vendorId);
 
-    //All "Accept" buttons have a data-id that matches the vendorId
-    $(".vendorAcceptBtn").attr("data-id", vendorId);
+//All "Accept" buttons have a data-id that matches the vendorId
+$(".vendorAcceptBtn").attr("data-id", vendorId);
 
-    //When any "Accept" button is clicked...
-    $(".vendorAcceptBtn").on("click", function () {
-        console.log("VENDOR ID INSIDE THE CLICK FUNCTION: " + vendorId);
-        console.log("I've been clicked");
+//When any "Accept" button is clicked...
+$(".vendorAcceptBtn").on("click", function () {
+  console.log("VENDOR ID INSIDE THE CLICK FUNCTION: " + vendorId);
+  console.log("I've been clicked");
 
-        var eventId = $(this).attr('event-id');
+  var eventId = $(this).attr("event-id");
 
-        console.log("VENDOR ID: " + vendorId);
-        console.log("EVENT ID: " + eventId);
+  console.log("VENDOR ID: " + vendorId);
+  console.log("EVENT ID: " + eventId);
 
 
-        $.ajax("/api/event/" + eventId, {
-            type: "PUT",
-            data: vendorId
-        }).then(function (res) {
-            console.log("AJAX CALL CAME BACK");
-            console.log(res);
-        });
-    });
+  $.ajax("/api/event/" + eventId, {
+    type: "PUT",
+    data: vendorId
+  }).then(function (res) {
+    console.log("AJAX CALL CAME BACK");
+    console.log(res);
+  });
+});
 
-    $(".moreInfoBtn").on("click", function () {
-        console.log("I've been clicked");
-        var btnID = ($(this).attr('data-id'));
-        console.log(btnID);
-        window.location.href = "/event/" + btnID;
-    });
+$(".moreInfoBtn").on("click", function () {
+  console.log("I've been clicked");
+  var btnID = ($(this).attr("data-id"));
+  console.log(btnID);
+  window.location.href = "/event/" + btnID;
+});
 
 // });
