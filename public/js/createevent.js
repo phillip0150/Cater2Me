e9cea8ca399ba33e0f9a4ae90019e8b5f65b49bc
lukas simianer <lsimianer@gmail.com>
2019-07-19 12:55:08 -0500

    city input capitalized

--- public/js/createevent.js
+++ public/js/createevent.js
@@ -19,6 +19,11 @@ $(function() {
 
   //if they click on the button in the create vendor form, 
   //create var to store values
+
+
+
+
+
   //send those values to /api/createVendor
   $(".create-event").on("submit", function(event) {
     event.preventDefault();
